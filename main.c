6a00a071b6185a3e46d9372611b23d144e5b5901
Mateus de Barros <97922225+MateusDBarros@users.noreply.github.com>
2024-10-27 03:19:39 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
-    char palavra[15] = "banana";
+    srand(time(0));
+    int random = rand() % 4;
+    char palavras[15] = {"banana", "carro", "arvore", "hospital"};
+    char palavra[] = palavras[random];
     int tamanho = strlen(palavra);
     char secreta[tamanho];
     int tentativas = 6;
@@ -15,19 +19,33 @@ int main(void)
     }
     secreta[tamanho] = '\0';
 
-    printf("%s", secreta);
-    printf("\n");
+    
     do {
+        int check = 0;
+        printf("%s\t Tentativas: %d\n", secreta, tentativas);
+        printf("\n");
         printf("digite uma letra: ");
         scanf(" %c", &letra);
         getchar();
         for (int i = 0; i < tamanho; i++) {
             if (letra == palavra[i]) {
                 secreta[i] = palavra[i];
+                check = 1;
             }
-        } 
-        printf("%s", secreta);
-        printf("\n");
-
+        }
+        if(check == 0) {
+            printf("Letra nao esta na palavra!\n");
+            tentativas--;
+        }
+        if (strcmp(palavra, secreta) == 0) {
+            printf("%s\t Tentativas: %d\n", palavra, tentativas);
+            printf("Voce venceu!\n");
+            return 0;
+        }
+        
     }while (tentativas != 0);
+
+    printf("\n");
+    printf("Que pena, tentativas acabaram, mais sorte da proxima vez!\n");
+    return 0;
 }
